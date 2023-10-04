aee1cf1ca9c4a565097142837646e6b103706ab5
Chris Dopuch <chris.dopuch@gmail.com>
2023-10-04 10:43:35 -0700

    cleanup home and about, more notes

--- pages/about.tsx
+++ pages/about.tsx
@@ -4,7 +4,14 @@ import Layout from "../components/Layout";
 const AboutPage = () => (
   <Layout title="About | Developer Dashboard">
     <h1>About</h1>
-    <p>This is the about page</p>
+    <p>This site is a developer dashboard for Company ABC.</p>
+    <p>
+      Github repo:{" "}
+      <a href="https://github.com/chrisdopuch/take-home-exercise-10-23">
+        Developer Dashboard
+      </a>
+    </p>
+    <p>Contributions are welcome!</p>
     <p>
       <Link href="/">Go home</Link>
     </p>
--- pages/index.tsx
+++ pages/index.tsx
@@ -3,10 +3,18 @@ import Layout from "../components/Layout";
 
 const IndexPage = () => (
   <Layout title="Home | Developer Dashboard">
-    <h1>Hello Next.js 👋</h1>
-    <p>
-      <Link href="/about">About</Link>
-    </p>
+    <h1>Company ABC Developer Dashboard Site</h1>
+    <ul>
+      <li>
+        <Link href="/about">About</Link>
+      </li>
+      <li>
+        <Link href="/services">Services</Link>
+      </li>
+      <li>
+        <Link href="/onCallEmployees">Employees</Link>
+      </li>
+    </ul>
   </Layout>
 );
 
